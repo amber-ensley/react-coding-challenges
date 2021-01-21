7eb758351a6f6601c09eb9cd8774d1359c94dbb7
amber.ensley <amber.ensley@quotumtech.com>
2021-01-21 12:56:54 -0500

    added client id and secret and created api calls in componentdidmount for browse component

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: 'fa054e58245e47fd93c03e08cbc4f722',
+    clientSecret: '4c03efcd954e4a7390db2ae0982ea60a'
   }
 }
--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
+import makeRequest from '../api/makeRequest';
 
 export default class Discover extends Component {
   constructor() {
     super();
 
     this.state = {
+      paths: ['new-releases', 'featured-playlists', 'categories'],
       newReleases: [],
       playlists: [],
       categories: []
     };
+  };
+
+  componentDidMount() {
+    this.state.paths.forEach(path => {
+      return makeRequest(path).then(data => {
+        switch(path) {
+          case 'new-releases':
+            this.setState({newReleases: data.data.albums.items});
+            break;
+          case 'featured-playlists':
+            this.setState({playlists: data.data.playlists.items});
+            break;
+          case 'categories':
+            this.setState({categories: data.data.categories.items});
+            break;
+          default: return;
+        }
+      })
+      
+    })
+    
   }
 
   render() {
